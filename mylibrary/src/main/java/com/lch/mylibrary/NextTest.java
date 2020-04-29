db1e0d8103c1f70d7fd2ce96790e323f5c1998c6
LCHKong <765353128@qq.com>
2020-04-29 13:55:11 +0800

    test first

---
+++ mylibrary/src/main/java/com/lch/mylibrary/NextTest.java
@@ -0,0 +1,12 @@
+package com.lch.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by LCH on 2020/4/29 13:53.
+ */
+public class NextTest {
+    public static void nextTest() {
+        Log.d("LCH-->>", "LCH");
+    }
+}
